ac13cad9adaa8c043f068a54fc53136afb040921
Matthew Blake <49418744+bmattblake@users.noreply.github.com>
2024-04-03 13:58:00 -0400

    mongodb implementation

--- db_conn.py
+++ db_conn.py
@@ -1,4 +1,6 @@
 
+import pymongo
+import certifi
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import os
@@ -6,20 +8,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 mongo_pass = os.getenv("mongodb_pass")
+print(mongo_pass)
 
-uri = f"mongodb+srv://bmattblake:admin@passdb.qcojh7t.mongodb.net/?retryWrites=true&w=majority&appName=PassDB"
+uri = f"mongodb+srv://bmattblake:{mongo_pass}@passdb.qcojh7t.mongodb.net/?retryWrites=true&w=majority&appName=PassDB"
+
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = pymongo.MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
 
 pass_man_collection = client.PassMan
 passdb = pass_man_collection.passdb
 
-passdb.insert_one({"website": "youtube", "username": "matthew", "password": "matthew's_password"})
+passdb.insert_one({"website": "youtube", 
+                   "username": "matthew",
+                   "password": "matthew's_password"})
 
 objects  = []
 for password in passdb.find():
     objects.append(password)
 
-print(objects[-1])
+print(objects)
 
